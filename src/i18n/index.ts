31bebe3f9362f1f59d8d972038ea638f57fca45f
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-05-11 19:10:12 +0100

    feat(i18n): add support for internationalization

---
+++ src/i18n/index.ts
@@ -0,0 +1,37 @@
+import i18next, {InitOptions} from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import {en} from './locales';
+
+const i18n = i18next;
+
+const options: InitOptions = {
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+  },
+  debug: true,
+  resources: {
+    en: {
+      common: en.en,
+    },
+  },
+
+  fallbackLng: 'en',
+
+  ns: ['common'],
+
+  defaultNS: 'common',
+
+  react: {
+    wait: false,
+    bindI18n: 'languageChanged loaded',
+    bindI18nStore: 'added removed',
+    nsMode: 'default',
+  },
+};
+
+i18n.use(LanguageDetector).init(options);
+i18n.changeLanguage(navigator.language, (err, t) => {
+  if (err) console.log(`Something went wrong during loading`);
+});
+
+export default i18n;
