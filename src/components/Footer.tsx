89ee112722d127144dfe2860850038040443aa54
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-05-20 18:37:07 +0100

    fix(footer): fix responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import {Image} from '@chakra-ui/image';
-import {Box, Container, Flex, Link, Text} from '@chakra-ui/layout';
+import {Box, Container, Flex, Link, Text, Grid} from '@chakra-ui/layout';
 import TelegramIcon from 'assets/svgs/telegram-s-icon.svg';
 import DiscordIcon from 'assets/svgs/discord-s-icon.svg';
 import FacebookIcon from 'assets/svgs/facebook-s-icon.svg';
@@ -62,18 +62,12 @@ const socialLinks = [
 
 const SocialLinks = () => {
   return (
-    <Flex
-      // w={'sm'}
-      grow={1}
-      direction={'row'}
-      justifyContent={'center'}
-      // justifyContent={{base: 'space-between', lg: 'space-around'}}
-    >
+    <Flex direction={'row'} mr={3}>
       {socialLinks.map((socialLink, index) => (
         <Link
           href={socialLink.href}
           isExternal={socialLink.isExternal}
-          mr={6}
+          mr={{base: 4, lg: 6}}
           key={index}>
           <Image
             w={6}
@@ -89,45 +83,39 @@ const SocialLinks = () => {
 
 export const Footer = () => {
   return (
-    <Container maxW={'8xl'} py={{base: 0, lg: 4}}>
+    <Container maxW={'full'} py={{base: 0, md: 4}}>
       <Flex
-        maxW={'full'}
-        direction={{base: 'column', lg: 'row'}}
+        flexDirection={{base: 'column', md: 'row'}}
         justifyContent={'space-between'}>
-        <Flex
-          grow={1}
-          direction={{base: 'column', lg: 'row'}}
-          justifyContent={'space-between'}
-          w={'xl'}
-          alignItems={{base: 'flex-start', lg: 'center'}}>
-          <Box flexGrow={1}>
+        <Flex flexGrow={2} direction={{base: 'column', md: 'row'}}>
+          <Box mr={{base: 0, md: 3}}>
             <Text fontWeight={'bold'} letterSpacing={'normal'}>
               ONTHER PTE.LTD.
             </Text>
           </Box>
-          <Box flexGrow={1}>
+          <Box mr={3} py={{base: 3, md: 0}}>
             <Text color={'gray.400'}>
               10 Anson Road #23-140 International Plaza Singapore
             </Text>
           </Box>
-          <Box flexGrow={1}>
+          <Box>
             <Text>info@onther.io</Text>
           </Box>
         </Flex>
-        <Flex
-          py={{base: 4, lg: 0}}
-          grow={3}
-          direction={{base: 'column', lg: 'row'}}
-          w={'sm'}
-          justifyContent={'center'}>
-          <SocialLinks />
+        <Box maxW={'full'}>
           <Flex
-            py={{base: 4, lg: 0}}
-            flexGrow={1}
-            justifyContent={{base: 'flex-start', lg: 'center'}}>
-            <Text>[Dropdown here]</Text>
+            py={{base: 4, md: 0}}
+            grow={2}
+            direction={{base: 'column', lg: 'row'}}
+            justifyContent={'flex-end'}>
+            <SocialLinks />
+            <Flex
+              py={{base: 4, lg: 0}}
+              justifyContent={{base: 'flex-start', lg: 'center'}}>
+              <Text>[Dropdown here]</Text>
+            </Flex>
           </Flex>
-        </Flex>
+        </Box>
       </Flex>
     </Container>
   );
