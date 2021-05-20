949a80f08bee53f89a8b3aede62686a3c41652a8
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-05-20 06:41:44 +0100

    feat(lint): add lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,16 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  printWidth: 100,
+  tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'all'
+  semi: true,
+  trailingComma: 'all',
+  arrowParens: "always",
+  overrides: [
+      {
+          files: '*.{js,jsx,tsx,ts,scss,json,html}',
+          options: {
+              tabWidth: 4,
+          },
+      },
+  ],
 };
