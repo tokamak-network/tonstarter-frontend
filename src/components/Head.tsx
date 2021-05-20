86b6b50156be976059cdf1fd7e2fd91cf9cb9bb0
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-05-20 05:50:52 +0100

    feat(seo): add react-helmet package

---
+++ src/components/Head.tsx
@@ -0,0 +1,22 @@
+import {FC} from 'react';
+import {Helmet} from 'react-helmet';
+
+type HeadProps = {
+  title?: string;
+  description?: string;
+};
+
+export const Head: FC<HeadProps> = ({title, description}) => {
+  const TITLE = `${title ? `${title} - ` : ''} Tokamak`;
+  return (
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content={description} />
+
+        <title>{TITLE}</title>
+      </Helmet>
+    </>
+  );
+};
