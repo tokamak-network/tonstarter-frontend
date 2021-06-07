5a0775c6b40baac1eb8cf29c14e8a993e5357ff1
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-06-07 10:41:48 +0100

    feat(staking): load all data at once

--- src/utils/timeStamp.ts
+++ src/utils/timeStamp.ts
@@ -1,40 +1,50 @@
 import {BigNumber, ethers} from 'ethers';
 import moment from 'moment';
-
 let provider = ethers.getDefaultProvider('rinkeby');
- 
 export const period = (startBlockNum: BigNumber, endBlockNum: BigNumber) => {
-let startBlock = Number(startBlockNum);
-let endBlock = Number(endBlockNum);
-
-let seconds = (endBlock - startBlock) * 13; 
-const periodHumanized = moment.duration(seconds, "seconds").humanize();
-
-    return periodHumanized;
-}
-
+  let startBlock = Number(startBlockNum);
+  let endBlock = Number(endBlockNum);
+  let seconds = (endBlock - startBlock) * 13;
+  const periodHumanized = moment.duration(seconds, 'seconds').humanize();
+  return periodHumanized;
+};
 export const formatStartTime = async (blockNum: BigNumber) => {
-    const blockNumber = Number(blockNum);
+  const blockNumberBN = BigNumber.from(blockNum);
+  const blockNumber = blockNumberBN.toNumber();
+  let currentBlock = await provider.getBlockNumber();
+  if (currentBlock > blockNumber) {
     const block = await provider.getBlock(blockNumber);
     const timeStamp = block.timestamp;
+    console.log(block.toString(), timeStamp);
     return moment.unix(timeStamp).format('MMM DD, YYYY HH:mm:ss');
-}
-
-export const formatEndTime = async (startBlockNum: BigNumber, endBlockNum: BigNumber) => {
-    let startB = Number(startBlockNum);
-    let endB = Number(endBlockNum);
-    let currentBlock = await provider.getBlockNumber();
-    
-    if (Number(currentBlock > endB)) {
-        const endBlock = await provider.getBlock(endB);
-        const timeStamp = endBlock.timestamp;
-        return moment.unix(timeStamp).format('MMM DD, YYYY HH:mm:ss');
-    }
-    else {
-        let seconds = (endB - startB) * 13;
-        const startBlock = await provider.getBlock(startB);
-        const startTimeStamp = startBlock.timestamp;
-        const timestamp = startTimeStamp + seconds; 
-        return moment.unix(timestamp).format('MMM DD, YYYY HH:mm:ss');
-    }
-}
+  } else {
+    let seconds = (blockNumber - currentBlock) * 13;
+    const currentBlk = await provider.getBlock(currentBlock);
+    const currentTimeStamp = currentBlk.timestamp;
+    const timestamp = currentTimeStamp + seconds;
+    return moment.unix(timestamp).format('MMM DD, YYYY HH:mm:ss');
+  }
+};
+export const formatEndTime = async (
+  startBlockNum: BigNumber,
+  endBlockNum: BigNumber,
+) => {
+  let startB = Number(startBlockNum);
+  let endB = Number(endBlockNum);
+  let currentBlock = await provider.getBlockNumber();
+  if (Number(currentBlock > endB)) {
+    const endBlock = await provider.getBlock(endB);
+    const timeStamp = endBlock.timestamp;
+    return moment.unix(timeStamp).format('MMM DD, YYYY HH:mm:ss');
+  } else {
+    let seconds = (endB - startB) * 13;
+    const currentBlk = await provider.getBlock(currentBlock);
+    const currentTimeStamp = currentBlk.timestamp;
+    let seconds2 = (startB - currentBlock) * 13;
+    const startTimeStamp = currentTimeStamp + seconds2;
+    // const startBlock = await provider.getBlock(startB);
+    // const startTimeStamp = startBlock.timestamp;
+    const timestamp = startTimeStamp + seconds;
+    return moment.unix(timestamp).format('MMM DD, YYYY HH:mm:ss');
+  }
+};
