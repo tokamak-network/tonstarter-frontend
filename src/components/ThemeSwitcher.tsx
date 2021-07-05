4d9a66c5a40fde4eadc80ef9b28c3b3daf297ef8
Youngsung <sysgigigi@gmail.com>
2021-07-05 14:32:44 +0900

    Feat : add new logo on the header

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -6,28 +6,35 @@ import {
   IconButtonProps,
   useTheme,
 } from '@chakra-ui/react';
-import {MoonIcon,
-  SunIcon} from '@chakra-ui/icons';
+import {MoonIcon, SunIcon} from '@chakra-ui/icons';
+import {useRouteMatch} from 'react-router-dom';
 
 type ThemeSwitcherProps = Omit<IconButtonProps, 'aria-label'>;
 
-export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = props => {
+export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = (props) => {
   const {toggleColorMode, colorMode} = useColorMode();
   const text = useColorModeValue('dark', 'light');
   const SwitchIcon = useColorModeValue(MoonIcon, SunIcon);
   const theme = useTheme();
+  const match = useRouteMatch('/');
 
   return (
     <IconButton
       size="md"
       fontSize="lg"
       variant="unstyled"
-      color={colorMode==='light'? 'white.100':theme.colors.white[100]}
+      color={
+        colorMode === 'light'
+          ? match?.isExact
+            ? 'white.100'
+            : 'black.200'
+          : theme.colors.white[100]
+      }
       marginLeft="2"
       onClick={toggleColorMode}
       bg={'transparent'}
       icon={<SwitchIcon />}
-      _hover={{ color: colorMode==='light'? '#ffff07' : 'red' }}
+      _hover={{color: colorMode === 'light' ? '#ffff07' : 'red'}}
       aria-label={`Switch to ${text} mode`}
       {...props}
     />
