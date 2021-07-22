4585e61e7071849feccb176a181387db5f636863
cd4761 <cd4761@gmail.com>
2021-07-22 14:06:58 +0900

    for test

--- src/pages/Staking/StakeOptionModal/Manage/index.tsx
+++ src/pages/Staking/StakeOptionModal/Manage/index.tsx
@@ -29,6 +29,7 @@ export const ManageModal = () => {
   const {colorMode} = useColorMode();
   let balance = data?.data?.stakeContractBalanceTon;
   let closed: any;
+  console.log(data?.data)
 
   try {
     closed = data?.data?.saleClosed;
