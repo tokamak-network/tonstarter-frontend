9f43d8879491221d45fd8f77e415ceddfb05404e
Youngsung <sysgigigi@gmail.com>
2021-07-23 15:43:01 +0900

    .

---
+++ src/hooks/useGraphQueries.ts
@@ -0,0 +1,6 @@
+// import {useQuery} from '@apollo/client';
+
+// export function useGraphQueries(query: any) {
+//   const {loading, error, data} = useQuery(query);
+//   return {loading, error, data};
+// }
