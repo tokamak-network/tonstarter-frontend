3be31a6b3e17632478eaf8613839400abec5dffa
Youngsung <sysgigigi@gmail.com>
2021-08-09 22:49:51 +0900

    DEPLOY - MANAGE

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -23,6 +23,7 @@ export interface RouterProps extends HTMLAttributes<HTMLDivElement> {}
 ###################
 ###PHASE 2 EALRY###
 ######MANAGE#######
+##NOT AIRDROP YET##
 ################### 
 */
 
