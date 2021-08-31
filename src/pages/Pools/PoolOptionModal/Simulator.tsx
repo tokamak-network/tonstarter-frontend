1b5c75c4ed43d8e41009645d21caea97b02a90ee
Youngsung <sysgigigi@gmail.com>
2021-08-31 12:56:18 +0900

    Temp

--- src/pages/Pools/PoolOptionModal/Simulator.tsx
+++ src/pages/Pools/PoolOptionModal/Simulator.tsx
@@ -1,3 +1,87 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Flex,
+  Stack,
+  useTheme,
+  useColorMode,
+} from '@chakra-ui/react';
+import {claim} from '../actions';
+import {useAppDispatch, useAppSelector} from 'hooks/useRedux';
+import {closeModal, selectModalType} from 'store/modal.reducer';
+import {useCallback, useState} from 'react';
+import {CloseButton} from 'components/Modal/CloseButton';
+import {useUser} from 'hooks/useUser';
+
 export const Simulator = () => {
-  return <div></div>;
+  const {account, library} = useUser();
+  const theme = useTheme();
+  const {colorMode} = useColorMode();
+
+  const {data} = useAppSelector(selectModalType);
+  const dispatch = useAppDispatch();
+
+  const handleCloseModal = useCallback(() => {
+    dispatch(closeModal());
+  }, [dispatch]);
+
+  return (
+    <Modal
+      isOpen={data.modal === 'pool_simulator' ? true : false}
+      isCentered
+      onClose={handleCloseModal}>
+      <ModalOverlay />
+      <ModalContent
+        fontFamily={theme.fonts.roboto}
+        bg={colorMode === 'light' ? 'white.100' : 'black.200'}
+        w="350px"
+        pt="25px"
+        pb="25px">
+        <CloseButton closeFunc={handleCloseModal}></CloseButton>
+        <ModalBody p={0}>
+          <Box
+            pb={'1.250em'}
+            borderBottom={
+              colorMode === 'light' ? '1px solid #f4f6f8' : '1px solid #373737'
+            }>
+            <Heading
+              fontSize={'1.250em'}
+              fontWeight={'bold'}
+              fontFamily={theme.fonts.titil}
+              color={colorMode === 'light' ? 'gray.250' : 'white.100'}
+              textAlign={'center'}>
+              Claim
+            </Heading>
+            <Text color="gray.175" fontSize={'0.750em'} textAlign={'center'}>
+              You earned {data?.data.earned ? data?.data.earned : '0.00'} TOS
+            </Text>
+          </Box>
+
+          <Stack
+            as={Flex}
+            justifyContent={'center'}
+            alignItems={'center'}
+            borderBottom={
+              colorMode === 'light' ? '1px solid #f4f6f8' : '1px solid #373737'
+            }
+            mb={'25px'}>
+            <Box textAlign={'center'} pt="20px" pb="20px">
+              <Text
+                fontSize={'26px'}
+                fontWeight={600}
+                color={colorMode === 'light' ? 'gray.250' : 'white.100'}>
+                TOS
+              </Text>
+            </Box>
+          </Stack>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
 };
