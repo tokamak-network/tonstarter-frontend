5326193c737e22bb92a880ba62c3cbde037346ac
Youngsung <sysgigigi@gmail.com>
2021-09-07 15:09:12 +0900

    Fix : useBlock

--- src/hooks/useBlock.ts
+++ src/hooks/useBlock.ts
@@ -1,36 +1,22 @@
+import {useWeb3React} from '@web3-react/core';
 import {useEffect, useState} from 'react';
-import Web3 from 'web3';
 
 export function useBlockNumber(): {blockNumber: number} {
-  const [blockNumber, setBlockNumber] = useState(0);
-  const [BN, setBN] = useState(0);
-
-  const web3 = new Web3((window as any).ethereum);
-  // const currentBlockNumber = web3.eth.getBlockNumber();
-  // currentBlockNumber.then((e) => setBlockNumber(e));
-
-  web3.eth
-    .subscribe('newBlockHeaders', function (error, result) {
-      if (!error) {
-        // const {number} = result;
-        // setBN(number);
-
-        return;
-      }
-
-      console.error(error);
-    })
-    // .on('connected', function (subscriptionId) {
-    //   console.log(subscriptionId);
-    // })
-    // .on('data', function (blockHeader) {
-    //   console.log(blockHeader);
-    // })
-    .on('error', console.error);
+  const [blockNumber, setBlockNumber] = useState<number>(0);
+  const {library} = useWeb3React();
 
   useEffect(() => {
-    setBlockNumber(BN);
-  }, [BN]);
+    if (library) {
+      library.getBlockNumber().then((bn: any) => {
+        setBlockNumber(bn);
+      });
+      library.on('block', setBlockNumber);
+      return () => {
+        library.removeListener('block', setBlockNumber);
+        setBlockNumber(0);
+      };
+    }
+  }, [library]);
 
   return {blockNumber};
 }
