1187e91fc57fb23f61fd8e5bdbb9741dfe3df188
SEFI2 <kadirpili@gmail.com>
2021-09-08 11:59:22 +0600

    Fix estimate reward logic

--- src/pages/Dao/components/utils/index.ts
+++ src/pages/Dao/components/utils/index.ts
@@ -2,5 +2,7 @@ import {stakeTOS} from './stakeTOS';
 import {unstakeTOS} from './unstakeTOS';
 import {increaseAmount} from './increaseAmount';
 import {extendPeriod} from './extendPeriod';
+import {getConstants} from './getConstants';
 
-export {stakeTOS, unstakeTOS, increaseAmount, extendPeriod};
+
+export {stakeTOS, unstakeTOS, increaseAmount, extendPeriod, getConstants};
