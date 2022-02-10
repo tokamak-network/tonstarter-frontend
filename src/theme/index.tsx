e4bd6a96398f205ad9c98ee17063ce59e299a5ae
christiansallen <christiansallen@gmail.com>
2022-02-09 21:34:10 -0700

    REWARD_STYLE mainText reverted to 20px fontSize

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -97,38 +97,38 @@ interface MENU_PROPS {
   colorMode: 'light' | 'dark';
 }
 export const MENU_STYLE = {
-buttonStyle: (props:MENU_PROPS) => ({
-  mr: '10px',
-  padding: '10px',
-  borderRadius: '4px',
-  h:'32px',
-  color: props.colorMode === 'light' ? '#3e495c' : '#f3f4f1',
-  fontSize:'12px',
-  w: '157px'
-}),
-buttonTextStyle: (props:MENU_PROPS) => ({
-  w:'100%',
-  display:'flex',
-  flexDir:'row',
-  alignItems:'center',
-  justifyContent:'space-between'
-}),
-menuListStyle: (props:MENU_PROPS) => ({
-  zIndex:10000,
-  m:'0px',
-  minWidth:"155px",
-  background: props.colorMode === 'light' ? '#ffffff' : '#222222'
-}),
-menuItemStyle: (props:MENU_PROPS) => ({
-  h:'30px',
-  color: props.colorMode === 'light' ? '#3e495c' : '#f3f4f1',
-  fontSize:'12px',
-  w:'155px',
-  m:'0px',
-  _hover:{background: 'transparent', color: 'blue.100'},
-   _focus:{background: 'transparent'}
-})
-}
+  buttonStyle: (props: MENU_PROPS) => ({
+    mr: '10px',
+    padding: '10px',
+    borderRadius: '4px',
+    h: '32px',
+    color: props.colorMode === 'light' ? '#3e495c' : '#f3f4f1',
+    fontSize: '12px',
+    w: '157px',
+  }),
+  buttonTextStyle: (props: MENU_PROPS) => ({
+    w: '100%',
+    display: 'flex',
+    flexDir: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  }),
+  menuListStyle: (props: MENU_PROPS) => ({
+    zIndex: 10000,
+    m: '0px',
+    minWidth: '155px',
+    background: props.colorMode === 'light' ? '#ffffff' : '#222222',
+  }),
+  menuItemStyle: (props: MENU_PROPS) => ({
+    h: '30px',
+    color: props.colorMode === 'light' ? '#3e495c' : '#f3f4f1',
+    fontSize: '12px',
+    w: '155px',
+    m: '0px',
+    _hover: {background: 'transparent', color: 'blue.100'},
+    _focus: {background: 'transparent'},
+  }),
+};
 
 // props.isUserTier === true
 //   ? '1px solid #0070ED'
@@ -153,9 +153,7 @@ export const REWARD_STYLE = {
     bg: props.colorMode === 'light' ? 'white.100' : '',
     borderRadius: 15,
     boxShadow: '0 2px 5px 0 rgba(61, 73, 93, 0.1)',
-    border: props.colorMode === 'light'
-      ? ''
-      : '1px solid #535353',
+    border: props.colorMode === 'light' ? '' : '1px solid #535353',
     // _hover: {border: 'solid 1px #0070ed'},
     // cursor: 'pointer',
     fontFamily: fonts.fld,
@@ -166,7 +164,7 @@ export const REWARD_STYLE = {
     color: props.colorMode === 'light' ? 'gray.250' : 'white.100',
   }),
   mainText: (props: REWARD_PROP) => ({
-    fontSize: props.fontSize || 28,
+    fontSize: props.fontSize || 20,
     fontWeight: 600,
     color: props.colorMode === 'light' ? 'gray.375' : 'white.100',
   }),
@@ -197,7 +195,7 @@ export const REWARD_STYLE = {
     }),
     percent: () => ({
       fontSize: 12,
-      color:'blue.100',
+      color: 'blue.100',
     }),
   },
   btn: (props: REWARD_PROP & {isActive: boolean}) => ({
@@ -558,7 +556,7 @@ const theme = extendTheme({
   REWARD_STYLE,
   STATER_STYLE,
   bgStyle,
-  MENU_STYLE
+  MENU_STYLE,
 });
 
 export default theme;
