b946208342950af40e9664bcd816da2bf3c2997b
Youngsung <sysgigigi@gmail.com>
2022-02-15 16:26:49 +0900

    Feat : DAO - dividend pool ready for deploy

--- src/pages/Dao/components/STOS.tsx
+++ src/pages/Dao/components/STOS.tsx
@@ -7,7 +7,9 @@ import {
   Grid,
   GridItem,
   Image,
+  Icon,
 } from '@chakra-ui/react';
+import {HamburgerIcon, CloseIcon} from '@chakra-ui/icons';
 import {AvailableBalance} from './AvailableBalance';
 import {MyStaked} from './MyStaked';
 import {MySTOS} from './MySTOS';
@@ -27,6 +29,11 @@ import * as ERC20 from 'services/abis/erc20ABI(SYMBOL).json';
 import {useBlockNumber} from 'hooks/useBlock';
 import useDate from '@Dao/hooks/useDate';
 import LIST_OPEN_IMAGE from 'assets/svgs/list-open_icon.svg';
+import LIST_OPEN_HOVER_IMAGE from 'assets/svgs/list-open_icon-hover.svg';
+import LIST_CLOSE_IMAGE from 'assets/svgs/list-close_icon.svg';
+import LIST_CLOSE_HOVER__IMAGE from 'assets/svgs/list-close_icon-hover.svg';
+import LIST_CLOSE_IMAGE_LIGHT from 'assets/svgs/list-close_icon_light.svg';
+
 import {motion} from 'framer-motion';
 
 type AirdropTokenList = {tokenName: string; amount: string}[];
@@ -65,6 +72,23 @@ const themeDesign = {
     dark: 'white.200',
   },
 };
+
+function getOpacity(index: number, arrLength: number): number[] {
+  const indexNum = Math.floor(index / 2);
+  const rowNum = Math.round(arrLength / 2);
+  const result = Array.from({length: rowNum}, (v, i) => {
+    return indexNum >= i ? 1 : 0;
+  });
+  return result;
+}
+
+function getTranslateY(index: number, arrLength: number): string[] {
+  const rowNum = Math.round(arrLength / 2);
+  const result = Array.from({length: rowNum + 1}, (v, i) =>
+    i === 0 ? '0px' : `-${35 * i}px`,
+  );
+  return result;
+}
 
 export const STOS = () => {
   const theme = useTheme();
@@ -152,17 +176,7 @@ export const STOS = () => {
         temp.push(tokenInfo);
       }
     });
-    // setAirdropExistingList(temp as AirdropTokenList);
-    setAirdropExistingList([
-      {tokenName: 'test', amount: '10,000.00'},
-      {tokenName: 'test2', amount: '100,000.00'},
-      {tokenName: 'test3', amount: '1,000,000.00'},
-      {tokenName: 'test4', amount: '1,000,000.00'},
-      {tokenName: 'test5', amount: '1,000,000.00'},
-      {tokenName: 'test5', amount: '1,000,000.00'},
-      {tokenName: 'test5', amount: '1,000,000.00'},
-    ]);
-
+    setAirdropExistingList(temp as AirdropTokenList);
     setTimeout(() => setLoading(false), 2500);
   }, [airdropList]);
 
@@ -224,10 +238,20 @@ export const STOS = () => {
           fontSize={'0.938em'}
           fontWeight={600}
           color={themeDesign.fontColorAddress[colorMode]}
-          zIndex={100}>
+          zIndex={1000}
+          w={'100%'}
+          bg={themeDesign.bg[colorMode]}
+          textAlign="center">
           Distributed List
         </Text>
-        <Text color={'gray.400'} fontSize={'0.8em'} pb={'1px'} zIndex={100}>
+        <Text
+          color={'gray.400'}
+          fontSize={'0.8em'}
+          pb={'1px'}
+          zIndex={1000}
+          w={'100%'}
+          bg={themeDesign.bg[colorMode]}
+          textAlign="center">
           Next(Thu.) {nextThu} 00:00:00 (UTC)
         </Text>
         <Flex
@@ -242,137 +266,217 @@ export const STOS = () => {
           mb={'52px'}
           color={themeDesign.fontAddressColor[colorMode]}>
           {loading === true && (
-            <Box h={'35px'} w={'100%'} textAlign="center" mt={'12px'}>
-              ...
-            </Box>
+            <Text h={'35px'} w={'100%'} textAlign="center" mt={'12px'}></Text>
           )}
           {loading === false && airdropExistingList?.length === 0 && (
-            <Text fontSize="0.8em" h={'35px'} mt={'12px'}>
+            <Text fontSize="0.8em" h={'35px'} mt={'12px'} zIndex={1100}>
               There isn't any distributed token
             </Text>
           )}
-          <Box
-            className="test"
-            w={'371px'}
-            h={'20px'}
-            pos="absolute"
-            zIndex={10000}
-            bg="white.100"></Box>
-          <Flex pos="relative" flexDir={'column'} mt={'12px'} h={'37px'}>
-            <Grid templateColumns={'repeat(2, 1fr)'} w={'371px'}>
-              {loading === false && (
-                <Image
-                  pos={'absolute'}
-                  right={0}
-                  mt={'10px'}
-                  src={LIST_OPEN_IMAGE}
-                  alt={'LIST_OPEN_IMAGE'}
-                  cursor="pointer"
-                  onClick={() => setViewAllTokens(!viewAllTokens)}></Image>
-              )}
-              {loading === false &&
-                airdropExistingList?.map((tokenInfo, index: number) => {
-                  // const indexNum = Math.round(index + 1 / 2);
-                  // console.log(indexNum);
-                  return (
-                    <motion.div
-                      // animate={index % 2 === 0 ? {translateY: '-18px'} : ''}
-                      animate={{
-                        translateY: [
-                          '0px',
-                          '-35px',
-                          '-70px',
-                          '-105px',
-                          '-140px',
-                        ],
-                        opacity: index < 2 ? [1, 0, 0, 0] : [1, 1, 1, 0],
-                        // translateY: '-35px',
-                        // opacity: 1
-                      }}
-                      transition={{
-                        duration: 12,
-                        delay: 3,
-                        repeat: Infinity,
-                      }}
-                      style={{zIndex: 10, position: 'relative'}}>
-                      <GridItem
-                      // display={
-                      //   index > 1 && viewAllTokens === false
-                      //     ? 'none'
-                      //     : 'block'
-                      // }
-                      >
-                        {/* {index !== 0 && (
-                      <span
-                        style={{
-                          fontSize: '1em',
-                          marginLeft: '10px',
-                          marginRight: '10px',
-                          textAlign: 'center',
-                          verticalAlign: 'center',
-                          lineHeight: '35px',
-                          color: '#86929d',
-                        }}>
-                        |
-                      </span>
-                    )} */}
-                        <Text
-                          fontFamily={theme.fonts.roboto}
-                          color={themeDesign.fontAddressColor[colorMode]}
-                          fontSize={'0.8em'}
-                          h={'35px'}
-                          textAlign="center"
-                          verticalAlign={'center'}
-                          lineHeight={'35px'}
-                          fontWeight={600}>
-                          {tokenInfo.amount}
-                          <span style={{marginLeft: '5px'}}>
-                            {tokenInfo.tokenName}
-                          </span>
-                        </Text>
-                      </GridItem>
-                    </motion.div>
-                  );
-                  // <GridItem>
-                  //   <Text
-                  //     fontFamily={theme.fonts.roboto}
-                  //     color={themeDesign.fontAddressColor[colorMode]}
-                  //     fontSize={'0.8em'}
-                  //     h={'35px'}
-                  //     textAlign="center"
-                  //     verticalAlign={'center'}
-                  //     lineHeight={'35px'}
-                  //     fontWeight={600}>
-                  //     {tokenInfo.amount}
-                  //     <span style={{marginLeft: '5px'}}>
-                  //       {tokenInfo.tokenName}
-                  //     </span>
-                  //   </Text>
-                  // </GridItem>
-                })}
-            </Grid>
-          </Flex>
+          {airdropExistingList && airdropExistingList.length > 0 && (
+            <Box
+              className="cover_token"
+              w={'371px'}
+              h={'20px'}
+              pos="absolute"
+              zIndex={1000}
+              bg={themeDesign.bg[colorMode]}></Box>
+          )}
+          {airdropExistingList && airdropExistingList.length > 0 && (
+            <Flex pos="relative" flexDir={'column'} mt={'12px'} h={'37px'}>
+              <Grid
+                templateColumns={'repeat(2, 1fr)'}
+                justifyContent="center"
+                w={'357px'}>
+                {loading === false && airdropExistingList.length > 2 && (
+                  <Icon
+                    as={viewAllTokens ? CloseIcon : HamburgerIcon}
+                    zIndex={10000}
+                    w={viewAllTokens ? '12px' : '16px'}
+                    h={viewAllTokens ? '12px' : '16px'}
+                    pos={'absolute'}
+                    right={'-10px'}
+                    mt={viewAllTokens ? '12px' : '10px'}
+                    alt={'LIST_OPEN_IMAGE'}
+                    cursor="pointer"
+                    color={colorMode === 'dark' ? '#ffffff' : ''}
+                    fontWeight={'bold'}
+                    _hover={{color: '#257eee'}}
+                    onClick={() => setViewAllTokens(!viewAllTokens)}></Icon>
+                )}
+                {loading === false &&
+                  viewAllTokens === false &&
+                  airdropExistingList?.map((tokenInfo, index: number) => {
+                    return (
+                      <motion.div
+                        animate={{
+                          translateY: getTranslateY(
+                            index,
+                            airdropExistingList.length,
+                          ),
+                          opacity: getOpacity(
+                            index,
+                            airdropExistingList.length,
+                          ),
+                        }}
+                        transition={{
+                          duration:
+                            Math.round(airdropExistingList.length / 2) * 2.5,
+                          delay: 1.5,
+                          repeat: Infinity,
+                        }}
+                        style={{zIndex: 10, position: 'relative'}}>
+                        <GridItem>
+                          {index % 2 !== 0 && (
+                            <span
+                              style={{
+                                position: 'absolute',
+                                left: '-10.8px',
+                                fontSize: '14px',
+                                marginLeft: '10px',
+                                marginRight: '10px',
+                                textAlign: 'center',
+                                verticalAlign: 'center',
+                                lineHeight: '35px',
+                                color: '#86929d',
+                              }}>
+                              |
+                            </span>
+                          )}
+                          <Text
+                            fontFamily={theme.fonts.roboto}
+                            color={themeDesign.fontAddressColor[colorMode]}
+                            fontSize={'18px'}
+                            h={'35px'}
+                            // pr={index % 2 !== 0 ? '' : '10px'}
+                            // pl={index % 2 !== 0 ? '10px' : ''}
+                            verticalAlign={'center'}
+                            lineHeight={'35px'}
+                            fontWeight={600}
+                            textAlign={
+                              index % 2 === 0 &&
+                              index + 1 === airdropExistingList.length
+                                ? 'center'
+                                : index % 2 !== 0
+                                ? 'left'
+                                : 'right'
+                            }
+                            pr={index % 2 !== 0 ? '' : '10px'}
+                            pl={index % 2 !== 0 ? '10px' : ''}
+                            width={
+                              index % 2 === 0 &&
+                              index + 1 === airdropExistingList.length
+                                ? '372px'
+                                : '178.5px'
+                            }
+                            position={
+                              index % 2 === 0 &&
+                              index + 1 === airdropExistingList.length
+                                ? 'absolute'
+                                : 'relative'
+                            }>
+                            {tokenInfo.amount}
+                            <span style={{marginLeft: '5px', fontSize: '13px'}}>
+                              {tokenInfo.tokenName}
+                            </span>
+                          </Text>
+                        </GridItem>
+                      </motion.div>
+                    );
+                  })}
+                {viewAllTokens === true &&
+                  airdropExistingList?.map((tokenInfo: any, index: number) => (
+                    <GridItem zIndex={1001}>
+                      {index % 2 !== 0 && (
+                        <span
+                          style={{
+                            position: 'absolute',
+                            left: '47%',
+                            fontSize: '14px',
+                            marginLeft: '10px',
+                            marginRight: '10px',
+                            textAlign: 'center',
+                            verticalAlign: 'center',
+                            lineHeight: '35px',
+                            color: '#86929d',
+                          }}>
+                          |
+                        </span>
+                      )}
+                      <Text
+                        fontFamily={theme.fonts.roboto}
+                        color={themeDesign.fontAddressColor[colorMode]}
+                        fontSize={'18px'}
+                        h={'35px'}
+                        verticalAlign={'center'}
+                        lineHeight={'35px'}
+                        fontWeight={600}
+                        textAlign={
+                          index % 2 === 0 &&
+                          index + 1 === airdropExistingList.length
+                            ? 'center'
+                            : index % 2 !== 0
+                            ? 'left'
+                            : 'right'
+                        }
+                        pr={index % 2 !== 0 ? '' : '10px'}
+                        pl={index % 2 !== 0 ? '10px' : ''}
+                        width={
+                          index % 2 === 0 &&
+                          index + 1 === airdropExistingList.length
+                            ? '372px'
+                            : '178.5px'
+                        }
+                        position={
+                          index % 2 === 0 &&
+                          index + 1 === airdropExistingList.length
+                            ? 'absolute'
+                            : 'relative'
+                        }>
+                        {tokenInfo.amount}
+                        <span style={{marginLeft: '5px', fontSize: '13px'}}>
+                          {tokenInfo.tokenName}
+                        </span>
+                      </Text>
+                    </GridItem>
+                  ))}
+              </Grid>
+            </Flex>
+          )}
+          {airdropExistingList && airdropExistingList.length > 0 && (
+            <Box
+              className="conver_token"
+              w={'371px'}
+              h={'400px'}
+              pos="absolute"
+              top={'400px'}
+              zIndex={1000}
+              bg={themeDesign.bg[colorMode]}></Box>
+          )}
         </Flex>
       </Flex>
-      <Box h={'68px'}>
+      <Box h={'68px'} zIndex={1000}>
         <AvailableBalance></AvailableBalance>
       </Box>
-      <Box h={'68px'}>
+      <Box h={'68px'} zIndex={1000}>
         <MyStaked></MyStaked>
       </Box>
-      <Box h={'68px'}>
+      <Box h={'68px'} zIndex={1000}>
         <MySTOS></MySTOS>
       </Box>
       <Box
         w={'100%'}
         h={'1px'}
+        zIndex={1000}
         bg={colorMode === 'light' ? '#e7edf3' : '#373737'}
         mt={'16px'}
         mb={'25px'}></Box>
-      <Box h={'68px'}>
+      <Box h={'68px'} zIndex={1000}>
         <Claim></Claim>
       </Box>
-      <Distribute></Distribute>
+      <Box zIndex={1000}>
+        <Distribute></Distribute>
+      </Box>
     </Flex>
   );
 };
