51e11b86eb26471c4086efc730f28125b32b791d
Christian Allen <christiansallen@gmail.com>
2022-05-04 19:44:19 -0600

    TS-109: Text color styling

--- src/pages/Launch/components/Projects/WtonTosLpReward.tsx
+++ src/pages/Launch/components/Projects/WtonTosLpReward.tsx
@@ -51,49 +51,66 @@ export const WtonTosLpReward: FC<WtonTosLpReward> = ({vault, project}) => {
       <Grid templateColumns="repeat(2, 1fr)" w={'100%'} mb={'30px'}>
         <Flex flexDirection={'column'} w={'60%'}>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             fontSize={'16px'}
             border={themeDesign.border[colorMode]}
             borderRight={'none'}
-            borderBottom={'none'}>
-            <Text>Token</Text>
-            <Text>
-              {commafy(Number(vault.vaultTokenAllocation))}{' '}
-              {project.tokenSymbol}
+            borderBottom={'none'}
+            color={colorMode === 'light' ? '#353c48' : '#fff'}>
+            <Text fontWeight={'bold'} fontSize={'15px'}>
+              Token
             </Text>
+            <Flex>
+              <Text letterSpacing={'1.3px'} fontSize={'13px'} mr={'5px'}>
+                {commafy(Number(vault.vaultTokenAllocation))}{' '}
+                {project.tokenSymbol}
+              </Text>
+              <Text letterSpacing={'1.3px'} fontSize={'13px'} color={'#7e8993'}>
+                (12.00 %)
+              </Text>
+            </Flex>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             border={themeDesign.border[colorMode]}
             borderRight={'none'}
-            borderBottom={'none'}>
+            borderBottom={'none'}
+            fontSize={'13px'}>
             <Text>Selected Pair</Text>
             <Text> {project.tokenSymbol} - TOS</Text>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             border={themeDesign.border[colorMode]}
             borderRight={'none'}
-            borderBottom={'none'}>
+            borderBottom={'none'}
+            fontSize={'13px'}>
             <Text>Pool Address</Text>
             <Text>
               {vault.poolAddress ? shortenAddress(vault.poolAddress) : 'N/A'}
             </Text>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             border={themeDesign.border[colorMode]}
             borderRight={'none'}
-            borderBottom={'none'}>
+            borderBottom={'none'}
+            fontSize={'13px'}>
             <Text>Vault Admin</Text>
             <Text>
               {vault.adminAddress ? shortenAddress(vault.adminAddress) : 'N/A'}
             </Text>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             border={themeDesign.border[colorMode]}
-            borderRight={'none'}>
+            borderRight={'none'}
+            fontSize={'13px'}>
             <Text>Vault Contract Address</Text>
             <Text>
               {vault.vaultAddress ? shortenAddress(vault.vaultAddress) : 'N/A'}
@@ -102,14 +119,17 @@ export const WtonTosLpReward: FC<WtonTosLpReward> = ({vault, project}) => {
         </Flex>
         <Flex flexDirection={'column'} ml={'-40%'}>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             border={themeDesign.border[colorMode]}
             borderBottom={'none'}>
-            <Text w={'40%'}>Liquidity Rewards Program Listed</Text>
-            <Flex w={'60%'} alignItems={'center'} justifyContent={'flex-end'}>
+            <Text w={'30%'} fontSize={'15px'}>
+              Liquidity Rewards Program Listed
+            </Text>
+            <Flex w={'70%'} alignItems={'center'} justifyContent={'flex-end'}>
               <Flex flexDirection={'column'} mr={'20px'} textAlign={'right'}>
-                <Text>You can create rewards program on</Text>
-                <Text>Mar. 31, 2022 00:00:00 (KST)</Text>
+                <Text color={'#7e8993'}>You can create rewards program on</Text>
+                <Text fontSize={'14px'}>Mar. 31, 2022 00:00:00 (KST)</Text>
               </Flex>
               <Button
                 bg={'#257eee'}
@@ -124,17 +144,26 @@ export const WtonTosLpReward: FC<WtonTosLpReward> = ({vault, project}) => {
             </Flex>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
+            justifyContent={'flex-start'}
             border={themeDesign.border[colorMode]}
             borderBottom={'none'}>
-            <Text w={'15%'}>#10</Text>
+            <Text w={'17%'} fontSize={'18px'}>
+              #10
+            </Text>
             <Flex w={'40%'} flexDirection={'column'} mr={'50px'}>
               <Text color={'#7e8993'}>Reward Duration</Text>
               <Text>2021.03.09 13:25 - 2022.03.09 13:26</Text>
             </Flex>
             <Flex w={'25%'} flexDirection={'column'} mr={'20px'}>
-              <Text>Refundable Amount</Text>
-              <Text>10,000,000 TON</Text>
+              <Text color={'#7e8993'}>Refundable Amount</Text>
+              <Flex alignItems={'baseline'}>
+                <Text mr={'3px'} fontSize={'16px'}>
+                  10,000,000
+                </Text>{' '}
+                <Text>TON</Text>
+              </Flex>
             </Flex>
             <Button
               bg={'#257eee'}
@@ -148,28 +177,35 @@ export const WtonTosLpReward: FC<WtonTosLpReward> = ({vault, project}) => {
             </Button>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             justifyContent={'flex-start'}
             border={themeDesign.border[colorMode]}
             borderBottom={'none'}>
-            <Text w={'15%'}>#10</Text>
+            <Text w={'15%'} fontSize={'18px'}>
+              #10
+            </Text>
             <Flex flexDirection={'column'}>
               <Text color={'#7e8993'}>Reward Duration</Text>
               <Text>2021.03.09 13:25 - 2022.03.09 13:26</Text>
             </Flex>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             justifyContent={'flex-start'}
             border={themeDesign.border[colorMode]}
             borderBottom={'none'}>
-            <Text w={'15%'}>#10</Text>
+            <Text w={'15%'} fontSize={'18px'}>
+              #10
+            </Text>
             <Flex flexDirection={'column'}>
               <Text color={'#7e8993'}>Reward Duration</Text>
               <Text>2021.03.09 13:25 - 2022.03.09 13:26</Text>
             </Flex>
           </GridItem>
           <GridItem
+            fontFamily={theme.fonts.fld}
             className={'chart-cell'}
             justifyContent={'center'}
             border={themeDesign.border[colorMode]}>
