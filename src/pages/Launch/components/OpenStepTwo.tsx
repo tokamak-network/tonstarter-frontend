739d9e90a3a8edbb396f8e517654370b92f7cb28
SonYoungsung <sysgigigi@gmail.com>
2022-05-16 15:16:47 +0700

    TSO-110 : Blind UX

--- src/pages/Launch/components/OpenStepTwo.tsx
+++ src/pages/Launch/components/OpenStepTwo.tsx
@@ -9,9 +9,12 @@ import VaultBasicSetting from '@Launch/components/modals/VaultBasicSetting';
 import TopTitle from '@Launch/components/stepTwo/TopTitle';
 import ClaimRound from '@Launch/components/stepTwo/ClaimRound';
 import Overview from '@Launch/components/stepTwo/Overview';
+import {useState} from 'react';
 
 const OpenStepTwo = () => {
   const {colorMode} = useColorMode();
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+
   return (
     <Flex
       w={'1100px'}
@@ -19,31 +22,46 @@ const OpenStepTwo = () => {
       border={colorMode === 'light' ? '' : '1px solid #373737'}
       borderRadius={'10px'}
       flexDir="column">
-      <TopTitle></TopTitle>
-      <Box mb={'20px'}>
-        <Line></Line>
-      </Box>
-      <Flex flexDir={'column'}>
-        <Vaults></Vaults>
-      </Flex>
-      <Box my={'25px'}>
-        <Line></Line>
+      <Box
+        w={'100%'}
+        bg={isEdit ? 'white.100' : 'none'}
+        zIndex={100}
+        opacity={isEdit ? 0.5 : 1}
+        pointerEvents={isEdit ? 'none' : 'all'}>
+        <TopTitle></TopTitle>
+        <Box mb={'20px'}>
+          <Line></Line>
+        </Box>
+        <Flex flexDir={'column'}>
+          <Vaults></Vaults>
+        </Flex>
+        <Box my={'25px'}>
+          <Line></Line>
+        </Box>
       </Box>
       <Flex px={'35px'}>
-        <Middle></Middle>
+        <Middle isEdit={isEdit} setIsEdit={setIsEdit}></Middle>
       </Flex>
-      <Box my={'25px'}>
-        <Line></Line>
+
+      <Box
+        w={'100%'}
+        bg={isEdit ? 'white.100' : 'none'}
+        zIndex={100}
+        opacity={isEdit ? 0.5 : 1}
+        pointerEvents={isEdit ? 'none' : 'all'}>
+        <Box my={'25px'}>
+          <Line></Line>
+        </Box>
+        <Flex px={'35px'}>
+          <ClaimRound></ClaimRound>
+        </Flex>
+        <Box my={'25px'}>
+          <Line></Line>
+        </Box>
+        <Flex px={'35px'} pb={'35px'}>
+          <Overview></Overview>
+        </Flex>
       </Box>
-      <Flex px={'35px'}>
-        <ClaimRound></ClaimRound>
-      </Flex>
-      <Box my={'25px'}>
-        <Line></Line>
-      </Box>
-      <Flex px={'35px'} pb={'35px'}>
-        <Overview></Overview>
-      </Flex>
       <VaultBasicSetting></VaultBasicSetting>
       <LaunchVaultPropModal></LaunchVaultPropModal>
       <CreateVaultModal></CreateVaultModal>
