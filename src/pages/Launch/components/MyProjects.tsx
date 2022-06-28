9eb8843fcc785cebc9990344f60fe15c948c2947
Lakmi <lakmi94@gmail.com>
2022-06-28 16:59:38 +0900

    fix launch page

--- src/pages/Launch/components/MyProjects.tsx
+++ src/pages/Launch/components/MyProjects.tsx
@@ -73,9 +73,16 @@ const MyProjects = () => {
         setProjectsForTable(datas);
         const projs = Object.keys(datas).map((k) => {
           const listed = starterData.rawData.some((el) => el.projectKey === k);
-          const stat = datas[k].vaults.every((vault: any) => {
-            return vault.isSet === true;
-          });
+          let stat
+          if (datas[k].vaults !== undefined) {
+            const statss = datas[k].vaults.every((vault: any) => {
+              return vault.isSet === true;
+            });
+            return stat = statss
+          }
+         else {
+          return stat = false
+         }
           const div = document.createElement('div');
           div.innerHTML = datas[k].description;
           const projectURIUnformatted = {
