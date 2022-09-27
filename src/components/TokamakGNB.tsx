11005c062d0b391155c925e061d5ae64cce406d6
Youngsung <sysgigigi@gmail.com>
2022-09-27 17:49:30 +0900

    Feat - Common GNB

--- src/components/TokamakGNB.tsx
+++ src/components/TokamakGNB.tsx
@@ -55,7 +55,7 @@ function TokamakGNB() {
       <Menu
         title={'Swap'}
         w={'76px'}
-        url={'https://swap.tokamak.network/'}></Menu>
+        url={'https://tonswapper.vercel.app/'}></Menu>
     </Flex>
   );
 }
