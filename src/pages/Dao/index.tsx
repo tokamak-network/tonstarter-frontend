e9f41da4609577eea004a93e2ba4db8315021b73
Youngsung <sysgigigi@gmail.com>
2022-09-30 12:04:13 +0900

    Fix - DAO : Notice

--- src/pages/Dao/index.tsx
+++ src/pages/Dao/index.tsx
@@ -20,6 +20,47 @@ import {
 import {Utility} from './components/Utility';
 import {DistributeModal} from 'pages/Admin/components/DistributeModal';
 
+const Notice = () => {
+  const theme = useTheme();
+  return (
+    <Flex flexDir={'column'} mt={'39px'}>
+      <Flex fontSize={20} fontWeight={'bold'}>
+        <Text color={'#2a72e5'}>[Notice]</Text>
+        <Text ml={'3px'}>
+          Planned service maintenance & smart contract upgrade announcement
+        </Text>
+      </Flex>
+      <Flex
+        fontSize={15}
+        flexDir={'column'}
+        mt={'15px'}
+        fontFamily={theme.fonts.fld}>
+        <Text>
+          Sometime between October ~ November 2022, TONStarter’s DAO page will
+          be deprecated and moved under the new TOSv2’s DAO page. TONStarter DAO
+          page’s “Manage”,
+        </Text>
+        <Text>
+          “Unstake”, “Stake” functionality will not work for up to 24 hours to
+          upgrade the TOSv2 staking contract. The exact schedule will be updated
+          later.
+        </Text>
+        <Flex>
+          <Text>Check</Text>
+          <Link
+            mx={'4px'}
+            color={'#2a72e5'}
+            isExternal={true}
+            href="https://tonstarter.tokamak.network/dao">
+            https://tonstarter.tokamak.network/dao
+          </Link>
+          <Text>for more details.</Text>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
 export const DAO = () => {
   const theme = useTheme();
   const {colorMode} = useColorMode();
@@ -41,107 +82,79 @@ export const DAO = () => {
 
   return (
     <Flex mb={'105px'}>
-      <Flex mt={theme.headerMargin.mt} w="100%" flexDir="column">
-        <Flex flexDir={'column'} pl={'18.9%'} mt={'39px'}>
-          <Flex fontSize={20} fontWeight={'bold'}>
-            <Text color={'#2a72e5'}>[Notice]</Text>
-            <Text ml={'3px'}>
-              Planned service maintenance & smart contract upgrade announcement
-            </Text>
-          </Flex>
-          <Flex
-            fontSize={15}
-            flexDir={'column'}
-            mt={'15px'}
-            fontFamily={theme.fonts.fld}>
-            <Text>
-              Sometime between October ~ November 2022, TONStarter’s DAO page
-              will be deprecated and moved under the new TOSv2’s DAO page.
-              TONStarter DAO page’s “Manage”,
-            </Text>
-            <Text>
-              “Unstake”, “Stake” functionality will not work for up to 24 hours
-              to upgrade the TOSv2 staking contract. The exact schedule will be
-              updated later.
-            </Text>
-            <Flex>
-              <Text>Check</Text>
-              <Link
-                mx={'4px'}
-                color={'#2a72e5'}
-                isExternal={true}
-                href="https://tonstarter.tokamak.network/dao">
-                https://tonstarter.tokamak.network/dao
-              </Link>
-              <Text>for more details.</Text>
-            </Flex>
-          </Flex>
-        </Flex>
-
-        <Flex justifyContent="center">
-          <Flex w={572} mr={158} mt={'60px'} flexDir="column">
-            <Box mb={'45px'}>
-              <Text
-                color={themeDesign.fontColor[colorMode]}
-                fontSize={'2.375em'}
-                fontWeight={'bold'}
-                mb="10px">
-                DAO
-              </Text>
-              <Text color={'gray.400'} fontSize={'1.000em'}>
-                Stake TOS and get sTOS. sTOS token is required to obtain the
-                rights for decision-making or sharing additional profit made
-                from the TONStarter platform.
-              </Text>
-            </Box>
-            <Flex>
-              <Container p={0} m={0} display="flex">
-                <Box mb="30px">
-                  <Text
-                    fontSize={'1.250em'}
-                    color={themeDesign.fontColor[colorMode]}
-                    mb={'10px'}
-                    fontWeight={600}>
-                    Governance
-                  </Text>
-                  <Text fontSize={'1em'} color={'gray.400'}>
-                    Go vote and and be an owner of TONStarter
-                  </Text>
-                </Box>
-                <Box
-                  w={160}
-                  h="38px"
-                  ml={'40px'}
-                  mt={'31px'}
-                  bg={themeDesign.bg[colorMode]}
+      <Flex
+        mt={theme.headerMargin.mt}
+        w="100%"
+        flexDir="column"
+        justifyContent={'center'}
+        alignItems={'center'}>
+        <Flex flexDir={'column'}>
+          <Notice></Notice>
+          <Flex>
+            <Flex w={572} mr={158} mt={'60px'} flexDir="column">
+              <Box mb={'45px'}>
+                <Text
                   color={themeDesign.fontColor[colorMode]}
-                  borderRadius={4}
-                  border={themeDesign.border[colorMode]}
-                  fontSize="0.813em"
-                  fontWeight={600}
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  px={15}
-                  cursor={'pointer'}
-                  _hover={{color: '#2a72e5', borderColor: '#2a72e5'}}>
-                  <Text
-                    onClick={(e: any) => {
-                      window.open(`https://snapshot.org/#/tonstarter.eth`);
-                    }}>
-                    Go to governance
-                  </Text>
-                  <Image src={resources_icon}></Image>
-                </Box>
-              </Container>
+                  fontSize={'2.375em'}
+                  fontWeight={'bold'}
+                  mb="10px">
+                  DAO
+                </Text>
+                <Text color={'gray.400'} fontSize={'1.000em'}>
+                  Stake TOS and get sTOS. sTOS token is required to obtain the
+                  rights for decision-making or sharing additional profit made
+                  from the TONStarter platform.
+                </Text>
+              </Box>
+              <Flex>
+                <Container p={0} m={0} display="flex">
+                  <Box mb="30px">
+                    <Text
+                      fontSize={'1.250em'}
+                      color={themeDesign.fontColor[colorMode]}
+                      mb={'10px'}
+                      fontWeight={600}>
+                      Governance
+                    </Text>
+                    <Text fontSize={'1em'} color={'gray.400'}>
+                      Go vote and and be an owner of TONStarter
+                    </Text>
+                  </Box>
+                  <Box
+                    w={160}
+                    h="38px"
+                    ml={'40px'}
+                    mt={'31px'}
+                    bg={themeDesign.bg[colorMode]}
+                    color={themeDesign.fontColor[colorMode]}
+                    borderRadius={4}
+                    border={themeDesign.border[colorMode]}
+                    fontSize="0.813em"
+                    fontWeight={600}
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    px={15}
+                    cursor={'pointer'}
+                    _hover={{color: '#2a72e5', borderColor: '#2a72e5'}}>
+                    <Text
+                      onClick={(e: any) => {
+                        window.open(`https://snapshot.org/#/tonstarter.eth`);
+                      }}>
+                      Go to governance
+                    </Text>
+                    <Image src={resources_icon}></Image>
+                  </Box>
+                </Container>
+              </Flex>
+              <Flex flexDir="column" w={636}>
+                <DAOStatistics></DAOStatistics>
+                <Utility></Utility>
+              </Flex>
             </Flex>
-            <Flex flexDir="column" w={636}>
-              <DAOStatistics></DAOStatistics>
-              <Utility></Utility>
+            <Flex mt={'57px'}>
+              <STOS></STOS>
             </Flex>
-          </Flex>
-          <Flex mt={'57px'}>
-            <STOS></STOS>
           </Flex>
         </Flex>
       </Flex>
