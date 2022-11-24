1fbe603ffa0450c521a3fbd6ea660992f2fd914e
Lakmi <lakmi94@gmail.com>
2022-11-24 16:48:02 +0900

    update header and footer

--- src/pages/Mobile/Components/MobileFooter.tsx
+++ src/pages/Mobile/Components/MobileFooter.tsx
@@ -1,16 +1,40 @@
 import {Box, Container, Flex, Link, Text, useTheme} from '@chakra-ui/react';
 import {useColorMode} from '@chakra-ui/react';
-import { useRouteMatch} from 'react-router-dom';
+import {useRouteMatch} from 'react-router-dom';
 export const MobileFooter = () => {
-    const {colorMode} = useColorMode();
-    const theme = useTheme();
-    const match = useRouteMatch('/');
-    return (
-        <Container  bg={match?.isExact? colorMode === 'light'? '#007aff' : '#222222' : 'transparent'}> 
-<Flex  h='50px' alignItems={'center'} justifyContent='space-between' fontFamily={theme.fonts.openSans} color={match?.isExact? '#fff' : colorMode === 'light'? '#3e495c' : '#ffffff'}>
-    <Text  fontSize='14px' fontWeight={800} >© 2022 Onther</Text>
-    <Text fontSize='11px' fontWeight={'normal'}>E. hello@tokamak.network</Text>
-</Flex>
-        </Container>
-    )
-}
+  const {colorMode} = useColorMode();
+  const theme = useTheme();
+  const match = useRouteMatch('/');
+  return (
+    <Flex
+      bg={
+        match?.isExact
+          ? colorMode === 'light'
+            ? '#007aff'
+            : '#222222'
+          : 'transparent'
+      }>
+      <Flex
+        h="50px"
+        alignItems={'center'}
+        justifyContent="space-between"
+        fontFamily={theme.fonts.openSans}
+        w='100%'
+        px='20px'
+        color={
+          match?.isExact
+            ? '#fff'
+            : colorMode === 'light'
+            ? '#3e495c'
+            : '#ffffff'
+        }>
+        <Text fontSize="14px" fontWeight={800}>
+          © 2022 Onther
+        </Text>
+        <Text fontSize="11px" fontWeight={'normal'}>
+          E. hello@tokamak.network
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
