c9fd52f34da90eb4cf48cae9a2941fbca1e7b1f9
Youngsung <sysgigigi@gmail.com>
2022-12-21 16:16:02 +0900

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -97,7 +97,7 @@ export const Footer = () => {
               Tokamak Network PTE.LTD
             </Text>
           </Box>
-          {/* <Box
+          <Box
             d="flex"
             justifyContent={'center'}
             alignItems={'center'}
@@ -110,7 +110,7 @@ export const Footer = () => {
               fontWeight={'normal'}>
               111 SOMERSET ROAD #06-07O 111 SOMERSET SINGAPORE 238164
             </Text>
-          </Box> */}
+          </Box>
           <Box
             d="flex"
             justifyContent={'center'}
