20fd04357458881e56e0ae141d4fb8426ec6d03c
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-27 19:02:27 +0900

    lint: Setup prettier lint packages, Configure ESLint rules for react & typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,32 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:prettier/recommended'
   ],
   overrides: [
   ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
-    'react'
+    'react',
+    '@typescript-eslint',
+    'prettier'
   ],
   rules: {
+    // Rules regarding react props
+    'react/prop-types': 'off',
+    // make sure hooks are used correctly; called at the top level of the component etc.
+    'react-hooks/rules-of-hooks': 'error',
+    // Disable 'React' library to import
+    'react/react-in-jsx-scope': 'off',
+    // If prettier rules exceptions
+    'prettier/prettier': ['warn'],
+    // Warn if function return type is missing
+    '@typescript-eslint/explicit-function-return-type': 'warn',
   }
 }
