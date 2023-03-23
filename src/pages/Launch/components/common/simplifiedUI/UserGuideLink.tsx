83b062d1a547aa78623c2fb66389f1bc7d834dba
Jananadi Wedagedara <jananadi92@gmail.com>
2023-03-23 18:44:22 +0900

    fix: User guide text color,style & margins

--- src/pages/Launch/components/common/simplifiedUI/UserGuideLink.tsx
+++ src/pages/Launch/components/common/simplifiedUI/UserGuideLink.tsx
@@ -1,14 +1,22 @@
-import { Link, Image, Text} from '@chakra-ui/react';
-import  UserGuideImage  from '../../../../../assets/svgs/sicon-user_guide.svg';
+import {Link, Image, Text} from '@chakra-ui/react';
+import UserGuideImage from '../../../../../assets/svgs/sicon-user_guide.svg';
+
 // TODO: import user guide url
 
 export const UserGuideLink = () => {
-    return (
+  return (
     <>
-    <Image ml={4} src={UserGuideImage} alt="sicon_user_guide" />
-    <Link>
-        <Text mt={2} ml={1} fontSize={13} color="gray.350">User Guide</Text>
-    </Link>
+      <Image ml={'21px'} src={UserGuideImage} alt="sicon_user_guide" />
+      <Link>
+        <Text
+          mt={2}
+          ml={'6px'}
+          fontSize={13}
+          color="gray.400"
+          fontFamily={'Titillium Web, sans-serif'}>
+          User Guide
+        </Text>
+      </Link>
     </>
-    )
-}
+  );
+};
