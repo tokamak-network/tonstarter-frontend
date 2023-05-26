50cf140c15da10062f70602a942e5d5b77df6964
Lakmi <lakmi94@gmail.com>
2023-05-26 14:30:58 +0900

    switch max and stableAmount

--- src/pages/Staking/StakeOptionModal/swap.tsx
+++ src/pages/Staking/StakeOptionModal/swap.tsx
@@ -247,7 +247,7 @@ export const SwapModal = () => {
                   h={'20px'}
                   fontSize={12}
                   fontWeight={600}
-                  onClick={() => setInputAmount(stableAmount.replace(/,/g, ''))}
+                  onClick={() => setInputAmount(max.replace(/,/g, ''))}
                   type={'button'}
                   variant="outline"
                   mr="6px"
@@ -262,7 +262,7 @@ export const SwapModal = () => {
                   h={'20px'}
                   fontSize={12}
                   fontWeight={600}
-                  onClick={() => setInputAmount(max.replace(/,/g, ''))}
+                  onClick={() => setInputAmount(stableAmount.replace(/,/g, ''))}
                   type={'button'}
                   variant="outline"
                   borderColor={colorMode === 'dark' ? '#535353' : '#9d9ea5'}
